4e16a3b6e8a3f6bbf10ce27a0940de316c60d04d
Однобурцев Вадим <odnoburtsev_vadim@cybersec74.ru>
2023-07-04 20:25:37 +0300

    Add project files.

---
+++ client/client.cpp
@@ -0,0 +1,122 @@
+﻿#include <iostream>
+#include <cstring>
+#include <thread>
+#include <chrono>
+#include <Ws2tcpip.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+constexpr int BUFFER_SIZE = 1024;
+constexpr int RECONNECT_INTERVAL = 10; // Период повторного подключения (в секундах)
+
+class Client {
+public:
+    Client(const std::string& serverIP, int serverPort)
+        : serverIP(serverIP), serverPort(serverPort), clientSocket(-1) {}
+
+    bool Connect() {
+#ifdef _WIN32
+        WSADATA wsData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) {
+            std::cout << "Не удалось инициализировать библиотеку Winsock." << std::endl;
+            return false;
+        }
+#endif
+
+        clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == -1) {
+            std::cout << "Не удалось создать сокет." << std::endl;
+            return false;
+        }
+
+        sockaddr_in serverAddress{};
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_port = htons(serverPort);
+        inet_pton(AF_INET, serverIP.c_str(), &(serverAddress.sin_addr));
+
+        if (connect(clientSocket, (sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+            std::cout << "Не удалось подключиться к серверу." << std::endl;
+            return false;
+        }
+
+        std::cout << "Успешное подключение к серверу." << std::endl;
+        return true;
+    }
+
+    void ReceiveAndPrint() {
+        while (true) {
+            if (clientSocket != -1) {
+                char buffer[BUFFER_SIZE];
+                memset(buffer, 0, sizeof(buffer));
+                int bytesRead = recv(clientSocket, buffer, BUFFER_SIZE - 1, 0);
+                if (bytesRead > 0) {
+                    std::cout << "Получено сообщение от сервера: " << buffer << std::endl;
+                }
+                else {
+                    std::cout << "Соединение с сервером разорвано. Повторная попытка подключения будет выполнена через "
+                        << RECONNECT_INTERVAL << " секунд." << std::endl;
+                    Reconnect();
+                }
+            }
+            else {
+                std::cout << "Не удалось установить соединение с сервером. Повторная попытка подключения будет выполнена через "
+                    << RECONNECT_INTERVAL << " секунд." << std::endl;
+                Reconnect();
+            }
+        }
+    }
+
+
+    void Reconnect() {
+        while (true) {
+            Disconnect();
+            std::this_thread::sleep_for(std::chrono::seconds(RECONNECT_INTERVAL));
+            if (Connect()) {
+                break;
+            }
+        }
+    }
+
+    void Disconnect() {
+        if (clientSocket != -1) {
+#ifdef _WIN32
+            closesocket(clientSocket);
+            WSACleanup();
+#else
+            close(clientSocket);
+#endif
+        }
+    }
+
+private:
+    std::string serverIP;
+    int serverPort;
+    int clientSocket;
+};
+
+int main() {
+    std::string serverIP = "127.0.0.1";
+    int serverPort = 8000;
+
+    Client client(serverIP, serverPort);
+
+    if (!client.Connect()) {
+        std::cout << "Не удалось подключиться к серверу. Повторная попытка подключения будет выполнена каждые " << RECONNECT_INTERVAL << " секунд." << std::endl;
+        client.Reconnect();
+    }
+
+    while (true) {
+        client.ReceiveAndPrint();
+    }
+
+    client.Disconnect();
+
+    return 0;
+}
---
+++ sockets/sockets.cpp
@@ -0,0 +1,176 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <string>
+#include <cstring>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#endif
+
+constexpr int MAX_CLIENTS = 10;
+constexpr int BUFFER_SIZE = 1024;
+
+// Структура, представляющая клиента
+struct Client {
+    int id;
+    int socket;
+    sockaddr_in address;
+
+    explicit Client(int id, int socket, sockaddr_in address)
+        : id(id), socket(socket), address(address) {}
+};
+
+// Класс сервера
+class Server {
+public:
+    Server(int port) : port(port) {
+        clients.reserve(MAX_CLIENTS);
+    }
+
+    void Start() {
+        if (!Initialize()) {
+            std::cout << "Ошибка инициализации сервера." << std::endl;
+            return;
+        }
+
+        std::thread acceptThread(&Server::AcceptClients, this);
+        std::thread commandThread(&Server::HandleCommands, this);
+
+        acceptThread.join();
+        commandThread.join();
+
+        Cleanup();
+    }
+
+private:
+    int port;
+    int serverSocket;
+    std::vector<Client> clients;
+    std::mutex clientsMutex;
+    std::condition_variable commandCV;
+    std::string command;
+
+    bool Initialize() {
+#ifdef _WIN32
+        WSADATA wsData;
+        if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) {
+            std::cout << "Не удалось инициализировать библиотеку Winsock." << std::endl;
+            return false;
+        }
+#endif
+
+        serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (serverSocket == -1) {
+            std::cout << "Не удалось создать сокет." << std::endl;
+            return false;
+        }
+
+        sockaddr_in serverAddress{};
+        serverAddress.sin_family = AF_INET;
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+        serverAddress.sin_port = htons(port);
+
+        if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == -1) {
+            std::cout << "Не удалось привязать сокет к адресу и порту." << std::endl;
+            return false;
+        }
+
+        if (listen(serverSocket, MAX_CLIENTS) == -1) {
+            std::cout << "Не удалось установить сокет в режим прослушивания." << std::endl;
+            return false;
+        }
+
+        std::cout << "Сервер запущен. Ожидание клиентов..." << std::endl;
+
+        return true;
+    }
+
+    void Cleanup() {
+#ifdef _WIN32
+        closesocket(serverSocket);
+        WSACleanup();
+#else
+        close(serverSocket);
+#endif
+    }
+
+    void AcceptClients() {
+        while (true) {
+            sockaddr_in clientAddress{};
+            int clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddress, nullptr);
+            if (clientSocket == -1) {
+                std::cout << "Ошибка при принятии клиента." << std::endl;
+                continue;
+            }
+
+            std::lock_guard<std::mutex> lock(clientsMutex);
+
+            int clientId = clients.size();
+            clients.emplace_back(clientId, clientSocket, clientAddress);
+
+            std::thread clientThread(&Server::HandleClient, this, clientId);
+            clientThread.detach();
+
+            std::cout << "Подключен новый клиент. ID: " << clientId << std::endl;
+        }
+    }
+
+    void HandleClient(int clientId) {
+        Client& client = clients[clientId];
+        char buffer[BUFFER_SIZE];
+
+        while (true) {
+            int bytesRead = recv(client.socket, buffer, BUFFER_SIZE, 0);
+            if (bytesRead <= 0) {
+                std::cout << "Клиент отключен. ID: " << clientId << std::endl;
+                break;
+            }
+
+            std::string message(buffer, bytesRead);
+            std::cout << "Получено сообщение от клиента " << clientId << ": " << message << std::endl;
+        }
+
+        std::lock_guard<std::mutex> lock(clientsMutex);
+        closesocket(client.socket);
+        clients.erase(clients.begin() + clientId);
+    }
+
+    void HandleCommands() {
+        while (true) {
+            std::string input;
+            std::getline(std::cin, input);
+
+            std::size_t pos = input.find(':');
+            if (pos != std::string::npos && input.substr(0, pos) == "sendto") {
+                int clientId = std::stoi(input.substr(pos + 1));
+                std::string command = input.substr(input.find(':', pos + 1) + 1);
+
+                std::lock_guard<std::mutex> lock(clientsMutex);
+                if (clientId >= 0 && clientId < clients.size()) {
+                    Client& client = clients[clientId];
+                    send(client.socket, command.c_str(), command.length(), 0);
+                }
+                else {
+                    std::cout << "Неверный идентификатор клиента." << std::endl;
+                }
+            }
+            else {
+                std::cout << "Некорректная команда." << std::endl;
+            }
+        }
+    }
+};
+
+int main() {
+    Server server(8000);
+    server.Start();
+    return 0;
+}
